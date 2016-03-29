ce2d626b172ee557e45a37cf4fcea691a88ae71a
李亚辉 <969898528@qq.com>
2016-03-29 10:27:09 +0800

    想上天

---
+++ app/src/main/java/com/example/dllo/newproject/ThreeActivity.java
@@ -0,0 +1,16 @@
+package com.example.dllo.newproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LYH on 16/3/29.
+ */
+public class ThreeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //我要上天
+    }
+}
